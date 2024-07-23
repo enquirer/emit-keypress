a36a06493cae93f5e353f0c973f5d33939a44441
Jon Schlinkert <jon.schlinkert@sellside.com>
2024-07-23 03:51:48 -0400

    handle slashes, and only use key name if it's printable

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,7 @@
 /* eslint-disable no-control-regex */
 import type readline from 'node:readline';
 
-export const PRINTABLE_CHAR_REGEX = /^[\p{L}\p{N}\p{P}\p{S}\p{Z}\p{Sm}]+$/u;
+export const PRINTABLE_CHAR_REGEX = /^[\p{L}\p{N}\p{P}\p{S}\p{Z}\p{Sm}/]+$/u;
 export const NON_PRINTABLE_CHAR_REGEX = /[^\P{Cc}\P{Cf}\p{L}\p{N}\p{P}\p{S}\p{Z}]/u;
 
 export const createShortcut = (key: readline.Key): string => {
@@ -10,7 +10,7 @@ export const createShortcut = (key: readline.Key): string => {
   if (key.ctrl) modifiers.push('ctrl');
   if (key.shift) modifiers.push('shift');
   if (key.meta) modifiers.push('meta');
-  let keyName = key.name || (isPrintableCharacter(key.sequence) ? key.sequence : '');
+  let keyName = isPrintableCharacter(key.sequence) ? key.sequence : key.name;
   if (keyName === 'undefined') keyName = '';
   return modifiers.length > 0 ? `${modifiers.join('+')}+${keyName}` : keyName;
 };
